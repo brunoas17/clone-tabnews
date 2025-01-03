85f00ff93dd68a02495e8c73330bdd0de4b6c769
Bruno André da Silva <38144738+brunoas17@users.noreply.github.com>
2025-01-03 20:18:24 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,8 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+
+  let result = null;
+  try {
+    result = await client.query(queryObject);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.end();
+  }
 
   return result;
 }

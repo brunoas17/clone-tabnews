ef0d574b22c7570435244002a86ae47fe42ef319
Bruno André da Silva <38144738+brunoas17@users.noreply.github.com>
2025-01-03 20:18:44 +0000

    finish `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,27 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const databaseServerVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
 
-  response
-    .status(200)
-    .json({ message: "Aprender no curso.dev me torna acima da média" });
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnection = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseServerVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: databaseOpenedConnection.rows[0].count,
+      },
+    },
+  });
 }
